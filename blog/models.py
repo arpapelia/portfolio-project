3069a74e90f949b4a319a3cbb1a546f94c9db1e0
arpapelia <arpapelia@gmail.com>
2018-08-31 18:43:31 +0300

    apps finished + migrated, and possible to add material in admin view

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=225)
+    pub_date = models.DateTimeField(editable=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
